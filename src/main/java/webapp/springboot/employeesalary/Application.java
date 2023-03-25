4f349be8ae363f973db1693c78bde19950e33823
norulshahlam <norulshahlam@gmail.com>
2023-03-26 05:30:57 +0800

    refactor

--- src/main/java/webapp/springboot/employeesalary/Application.java
+++ src/main/java/webapp/springboot/employeesalary/Application.java
@@ -3,6 +3,9 @@ package webapp.springboot.employeesalary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author NORUL
+ */
 @SpringBootApplication
 public class EmployeesalaryApplication {
 	
